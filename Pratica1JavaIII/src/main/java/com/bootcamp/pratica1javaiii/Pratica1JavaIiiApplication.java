522e4a7f85f4e61688d16cd47d9a77367f51701d
rosalia-oliveira <rosalia.oliveira@mercadolivre.com>
2022-10-05 11:23:29 -0300

    feat: add extras

---
+++ Pratica1JavaIII/src/main/java/com/bootcamp/pratica1javaiii/Pratica1JavaIiiApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.pratica1javaiii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pratica1JavaIiiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pratica1JavaIiiApplication.class, args);
+    }
+
+}
---
+++ Pratica1JavaIII/src/test/java/com/bootcamp/pratica1javaiii/Pratica1JavaIiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.pratica1javaiii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pratica1JavaIiiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
