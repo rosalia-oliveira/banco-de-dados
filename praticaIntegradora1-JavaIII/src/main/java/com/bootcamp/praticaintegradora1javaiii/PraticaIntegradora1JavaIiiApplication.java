2fe43d7f8bb009b27cd73431745215a312691232
rosalia-oliveira <rosalia.oliveira@mercadolivre.com>
2022-10-05 10:51:40 -0300

    feat: add extras

---
+++ praticaIntegradora1-JavaIII/src/main/java/com/bootcamp/praticaintegradora1javaiii/PraticaIntegradora1JavaIiiApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.praticaintegradora1javaiii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaIntegradora1JavaIiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticaIntegradora1JavaIiiApplication.class, args);
+	}
+
+}
---
+++ praticaIntegradora1-JavaIII/src/test/java/com/bootcamp/praticaintegradora1javaiii/PraticaIntegradora1JavaIiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.praticaintegradora1javaiii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticaIntegradora1JavaIiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
