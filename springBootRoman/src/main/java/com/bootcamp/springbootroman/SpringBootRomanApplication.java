6ab322a874283d70cd8ad9b93f52045c15a57e8c
rosalia-oliveira <rosalia.oliveira@mercadolivre.com>
2022-10-05 10:50:14 -0300

    feat: add extras

---
+++ springBootRoman/src/main/java/com/bootcamp/springbootroman/SpringBootRomanApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.springbootroman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRomanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRomanApplication.class, args);
+    }
+
+}
