3be8dfe3b5b359a01e50e2e059bf89f66fbb341e
rosalia-oliveira <rosalia.oliveira@mercadolivre.com>
2022-10-13 09:44:31 -0300

    feat: add modificacoes extras sem alteracoes

---
+++ pratica2TestingI/src/test/java/br/com/pratica2testingI/Pratica2TestingIApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pratica2testingI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pratica2TestingIApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
