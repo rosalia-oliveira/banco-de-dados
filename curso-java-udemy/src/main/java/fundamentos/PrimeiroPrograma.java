a67f82d32c063f3001cee0805843af5985adbae8
Ro-padoin <padoinrosalia@gmail.com>
2022-10-24 08:27:25 -0300

    :sparkles: feat: add exemplos curso java udemy

--- curso-java-udemy/src/main/java/fundamentos/PrimeiroPrograma.java
+++ curso-java-udemy/src/main/java/fundamentos/PrimeiroPrograma.java
@@ -1,2 +1,14 @@
-package fundamentos;public class PrimeiroPrograma {
+package fundamentos;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Primeiro programa!");
+    }
 }
+
+// comentário de 1 linha usa-se //
+/* comentário de mais de 1 linhas usam-se estes símbolos ou além destes símbolos adicione * em cada linha */
+
+// /**
+// * barra + ** e enter cria o Java Doc.
+// */
