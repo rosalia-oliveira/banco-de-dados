4d4f23477de538b5722b2c1941c4da12b6d12158
Ro-padoin <padoinrosalia@gmail.com>
2022-10-24 08:57:38 -0300

    :sparkles: feat: update branch master

--- CRUD/src/main/java/com/crud/storage/repository/SystemUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.crud.storage.repository;
-
-import com.crud.storage.model.SystemUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SystemUserRepository extends JpaRepository<SystemUser, Long> { }
