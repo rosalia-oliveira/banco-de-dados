abadb8245c0a87c46f4681b0819c696c67ffdef3
Ro-padoin <padoinrosalia@gmail.com>
2022-10-28 12:35:50 -0300

    :sparkles: feat: desafio conversao

---
+++ curso-java-udemy/src/main/java/desafios/conversao/DesafioConversao.java
@@ -0,0 +1,25 @@
+package desafios.conversao;
+
+import java.util.Scanner;
+
+public class DesafioConversao {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o primeiro valor de salario: ");
+        Double salaryOne = Double.parseDouble(input.nextLine().replace(",", "."));
+
+        System.out.println("Digite o segundo valor de salario: ");
+        Double salaryTwo = Double.parseDouble(input.nextLine().replace(",", "."));
+
+        System.out.println("Digite o terceiro valor de salario: ");
+        Double salaryThree = Double.parseDouble(input.nextLine().replace(",", "."));
+
+       Double average = (salaryOne + salaryTwo + salaryThree) / 3;
+
+        System.out.println("Média Salarial: " + average);
+
+        input.close();
+
+    }
+}
